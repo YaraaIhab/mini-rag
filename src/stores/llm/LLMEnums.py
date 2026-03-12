a54871f337dec3fc025d16d0e417306ec8d777fa
Yara <yara.ihabb133@gmail.com>
2026-03-12 16:50:49 +0200

    created enum for llms

--- src/stores/llm/LLMEnums.py
+++ src/stores/llm/LLMEnums.py
@@ -11,3 +11,17 @@ class OpenAIEnum(Enum):
     SYSTEM = "system"
     USER = "user"
     ASSISTANT = "assistant"  
+
+class CohereEnum(Enum):
+    SYSTEM = "SYSTEM"
+    USER = "USER"
+    ASSISTANT = "CHATBOT" 
+    
+    DOCUMENT = "search_document"
+    QUERY = "search_query"
+     
+
+class DocumentTypeEnum(Enum):
+    DOCUMENT = "document"
+    QUERY = "query"
+    
