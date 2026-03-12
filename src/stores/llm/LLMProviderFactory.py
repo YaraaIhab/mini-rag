55db8437bc6ad61ccf14a5a9594d9922fd1778b1
Yara <yara.ihabb133@gmail.com>
2026-03-12 16:51:40 +0200

    create llm factory

---
+++ src/stores/llm/LLMProviderFactory.py
@@ -0,0 +1,26 @@
+from .LLMEnums import LLMEnum
+from .providers import OpenAIProvider, CohereProvider
+
+class LLMProviderFactory:
+    def __init__(self, config: dict):
+        self.config = config
+    
+    def create_provider(self, provider:str):
+        if provider == LLMEnum.OPENAI.value:
+            return OpenAIProvider(
+                api_key = self.config.OPENAI_API_KEY,
+                api_url = self.config.OPENAI_API_URL,
+                default_input_max_characters = self.config.DEFAULT_INPUT_MAX_CHARACTERS,
+                default_output_max_tokens = self.config.DEFAULT_OUTPUT_MAX_TOKENS,
+                default_temperature = self.config.DEFAULT_TEMPERATURE
+            )
+        
+        elif provider == LLMEnum.COHERE.value:
+            return CohereProvider(
+                api_key = self.config.COHERE_API_KEY,
+                default_input_max_characters = self.config.DEFAULT_INPUT_MAX_CHARACTERS,
+                default_output_max_tokens = self.config.DEFAULT_OUTPUT_MAX_TOKENS,
+                default_temperature = self.config.DEFAULT_TEMPERATURE
+            )
+        return None
+
